0b86c38b6b659c9958b5642842f75fec59ee58a2
Radicheski <erik.radicheski@gmail.com>
2021-08-10 15:43:34 -0300

    Primeiro commit. Projeto limpo

---
+++ SwiflixReborn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiflixReborn
+//
+//  Created by Erik Radicheski da Silva on 10/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
