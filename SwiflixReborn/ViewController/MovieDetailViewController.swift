3dce39f6c8d53cecf1c33ccbcf349d7a11b60168
Radicheski <erik.radicheski@gmail.com>
2021-08-18 18:18:07 -0300

    Adicionada exibição de detalhes do filme selecionado na aba "Geral" da tela de detalhes

--- SwiflixReborn/ViewController/MovieDetailViewController.swift
+++ SwiflixReborn/ViewController/MovieDetailViewController.swift
@@ -38,6 +38,7 @@ class MovieDetailViewController: UIViewController {
     }
     
     func setupDetailTableView() {
+        self.detailTableView.register(UINib(nibName: "PersonBiographyTableViewCell", bundle: nil), forCellReuseIdentifier: PersonBiographyTableViewCell.customIdentifier)
         self.detailTableView.delegate = self
         self.detailTableView.dataSource = self
     }
@@ -54,7 +55,11 @@ class MovieDetailViewController: UIViewController {
         }
 
     }
-
+    
+    @IBAction func tabDidSelect(_ sender: UISegmentedControl) {
+        self.detailTableView.reloadData()
+    }
+    
 }
 
 extension MovieDetailViewController: UITableViewDelegate {
@@ -63,12 +68,71 @@ extension MovieDetailViewController: UITableViewDelegate {
 
 extension MovieDetailViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        switch self.tabSegment.selectedSegmentIndex {
+        case 0:
+            return 1
+        default:
+            return 0
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        switch self.tabSegment.selectedSegmentIndex {
+        case 0:
+            return 3
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch self.tabSegment.selectedSegmentIndex {
+        case 0:
+            
+            switch section {
+            case MovieGeneralSection.overview.rawValue: return "Overview"
+            case MovieGeneralSection.originalTitle.rawValue: return "Original title"
+            case MovieGeneralSection.releaseDate.rawValue: return "Release date"
+            default: return nil
+            }
+            
+        default:
+            return nil
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PersonBiographyTableViewCell.customIdentifier) as? PersonBiographyTableViewCell else { return UITableViewCell() }
+        switch self.tabSegment.selectedSegmentIndex {
+        case 0:
+            return self.getGeneralIInformation(cell, at: indexPath)
+        default:
+            return UITableViewCell()
+        }
     }
     
+    func getGeneralIInformation(_ cell: PersonBiographyTableViewCell, at indexPath: IndexPath) -> PersonBiographyTableViewCell{
+        
+        switch indexPath.section {
+        case MovieGeneralSection.overview.rawValue:
+            cell.setup(with: self.detail?.overview ?? "No overview available")
+        case MovieGeneralSection.originalTitle.rawValue:
+            cell.setup(with: self.detail?.originalTitle ?? "No orginal title avilable")
+        case MovieGeneralSection.releaseDate.rawValue:
+            cell.setup(with: self.detail?.releaseDate ?? "No release date available")
+        default:
+            cell.setup(with: "")
+        }
+        
+        return cell
+    }
+    
+}
+
+enum MovieGeneralSection: Int {
+    
+    case overview = 0
+    case originalTitle = 1
+    case releaseDate = 2
     
 }
