2cd1b27de12ace50717ef9859b21bdeae528fd42
Radicheski <erik.radicheski@gmail.com>
2021-08-27 16:25:06 -0300

    Criada função para autenticação do usuário na tela de login

--- SwiflixReborn/ViewController/LoginViewController.swift
+++ SwiflixReborn/ViewController/LoginViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var enterButton: UIButton!
@@ -9,6 +10,11 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let _ = Auth.auth().currentUser {
+            self.showMainScreen()
+        }
+        
     }
     
     @IBAction func enterClicked(_ sender: UIButton) {
@@ -20,6 +26,21 @@ class LoginViewController: UIViewController {
     }
     
     func authenticate() {
+        
+        if let email = self.emailField.text,
+           let password = self.passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { result, error in
+                if let _ = result {
+                    self.showMainScreen()
+                } else if let _ = error {
+                    #warning("Handle authentication error")
+                }
+            }
+        }
+        
+    }
+    
+    func showMainScreen() {
         self.performSegue(withIdentifier: "LoginToMain", sender: nil)
     }
     
